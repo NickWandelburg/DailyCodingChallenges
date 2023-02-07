14b2badcaa780c3deb8aeb6338022a63bf636f81
NickWandelburg <nick.wandelburg@icloud.com>
2023-02-07 18:03:52 +0100

    Day 7: Find Vertex of a Quadratic Function

---
+++ VertexOfQuadratic/__init__.py
@@ -0,0 +1,24 @@
+# Find Vertex of a Quadratic Function
+def find_vertex(a, b, c):
+    # solved derivative of quadratic function
+    x = -b / (2 * a)
+    # quadratic function at vertex
+    y = a * (x ** 2) + b * x + c
+    return x, y
+
+
+if __name__ == "__main__":
+    a = 1
+    b = -10
+    c = 4
+
+    x, y = find_vertex(a, b, c)
+    x, y = round(x, 2), round(y, 2)
+
+    if a == 1:
+        print(f"The vertex of y=x\u00b2{b:+}x{c:+} is at ({x}, {y})")
+    elif a == -1:
+        print(f"The vertex of y=-x\u00b2{b:+}x{c:+} is at ({x}, {y})")
+    else:
+        print(f"The vertex of y={a}x\u00b2{b:+}{c:+} is at ({x}, {y})")
+
